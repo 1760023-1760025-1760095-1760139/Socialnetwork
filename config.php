7dba3fe2d656440a0e62c51753f86ae491d88a6c
honglien82 <58362687+honglien82@users.noreply.github.com>
2019-12-27 21:08:26 +0700

    1760095

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    //DB heroku
+    'database' => [
+        'host' => 'remotemysql.com',
+        'name' => 'nQhrcXzBBL',
+        'user' => 'nQhrcXzBBL',
+        'pass' => 'GV65vJDdMj'
+    ],
+//    DB local
+//    'database' => [
+//        'host' => 'localhost',
+//        'name' => 'peace',
+//        'user' => 'root',
+//        'pass' => 'root'
+//    ],
+];
+
+?>
